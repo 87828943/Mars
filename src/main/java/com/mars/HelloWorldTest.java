440e8a8666aa14fafd8e7ea7dd7d5c178e4eef54
guowenbo <87828943@qq.com>
2018-06-01 17:40:12 +0800

    testhhhhhhhhhhhh

---
+++ src/main/java/com/mars/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package com.mars;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldTest {
+
+    @RequestMapping("/hello")
+    private String hello(){
+        return "hello";
+    }
+}
