e5e5e8f801ab15cf411e60e3a86384edd0c1b1fa
luckyMeteor <zhaoxingxing@gomeplus.com>
2018-06-13 17:45:28 +0800

    增加日志

--- src/main/java/com/mars/MarsApplication.java
+++ src/main/java/com/mars/MarsApplication.java
@@ -7,6 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MarsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MarsApplication.class, args);
+		long starTime = System.currentTimeMillis();
+        SpringApplication.run(MarsApplication.class,args);
+        long endTime=System.currentTimeMillis();
+        long Time=endTime-starTime;
+        System.out.println("\n启动时间:"+ Time/1000 +"秒");
+        System.out.println("...............................................................");
+        System.out.println("..................Service starts successfully..................");
+        System.out.println("...............................................................");
 	}
 }
