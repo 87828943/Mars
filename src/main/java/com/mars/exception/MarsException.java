c994b61b5a353b24fae89dae5171837f17189b60
guodada <87828943@qq.com>
2018-06-16 18:00:46 +0800

    commit
    Signed-off-by: guodada <87828943@qq.com>

--- src/main/java/com/mars/exception/MarsException.java
+++ src/main/java/com/mars/exception/MarsException.java
@@ -4,8 +4,13 @@ public enum MarsException {
 
     FAILED("99999","失败"),
     PARAM_EXCEPTION("00001","参数错误"),
-    EMAIL_EXIST("00002","邮箱已注册！"),
-    NAME_EXIST("00003","昵称已占用");
+
+    //user相关:100001
+    EMAIL_EXIST("10001","邮箱已注册！"),
+    NAME_EXIST("10002","昵称已占用"),
+
+    //test:200001
+    TEST("20001","测试占用");
 
     private String code;
     private String msg;
