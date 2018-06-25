980804dcce23c72651c2dc188feeca5e7772701a
guodada <87828943@qq.com>
2018-06-25 12:06:32 +0800

    commit
    Signed-off-by: guodada <87828943@qq.com>

--- src/main/resources/static/js/layout.js
+++ src/main/resources/static/js/layout.js
@@ -1,23 +1,21 @@
 $(function() {
-    locationUrl('test/test1','home');
+    locationUrl('main','home');
 });
 
 function logout(){
-	if(confirm("确认注销吗？")){
-		$.ajax({
-			url:"/user/logout",
-			type:"POST",
-			data:{},
-			success:function(res){
-				if(res.resCode == "00000"){
-					window.location.href="/index";
-				}
-			},
-			error:function(){
-				cofirm
+	$.ajax({
+		url:"/user/logout",
+		type:"POST",
+		data:{},
+		success:function(res){
+			if(res.resCode == "00000"){
+				window.location.href="/index";
 			}
-		});
-	}
+		},
+		error:function(){
+			cofirm
+		}
+	});
 }
 
 function editUser(){
