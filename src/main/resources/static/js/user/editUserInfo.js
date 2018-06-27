20d5323f56b855900e927b4d3cea8016c4075819
guodada <87828943@qq.com>
2018-06-27 19:09:43 +0800

    commit
    Signed-off-by: guodada <87828943@qq.com>

--- src/main/resources/static/js/user/editUserInfo.js
+++ src/main/resources/static/js/user/editUserInfo.js
@@ -1,29 +1,60 @@
 $(function(){
-	
+    validate();
 });
 
+function validate(){
+    $("#editUserInfoForm").bootstrapValidator({
+        message: '数据验证失败',
+        excluded: [':hidden'],
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            description: {
+                validators: {
+                    notEmpty: {
+                        message: '请填写描述！'
+                    },
+					stringLength: {
+                        max: 15,
+                        message: '不能超过15个字~'
+                    },
+                }
+            }
+        }
+    })
+}
+
 function editUserInfo(){
+
+    $("#editUserInfoForm").data('bootstrapValidator').destroy();
+    validate();
+    $("#editUserInfoForm").bootstrapValidator('validate');
+    if (!$("#editUserInfoForm").data('bootstrapValidator').isValid()) {
+        return false;
+    }
+
 	var sex = $("input[name='sex']:checked").val();
 	var description = $("#description").val();
-	$.ajax({
-		url:"user/updateUserInfo",
-		type:"POST",
-		data:{
-			sex:sex,
-			description:description
-		},
-		success:function(res){
-			console.log(res);
-			if(res.resCode == "00000"){
-				alert();
-				$('#updateSuccessModal').modal('show');
+
+    $.ajax({
+        url: "/user/updateUserInfo",
+        type: "POST",
+        data:{
+            sex:sex,
+            description:description
+        },
+        success: function(res) {
+            if(res.resCode == "00000"){
+                $('#updateSuccessModal').modal('show');
             }else{
                 $("#errorMsg").html(res.resMsg);
                 $('#updateFailedModal').modal('show');
             }
-		},
-		error:function(){
-			
-		}
-	})
+        },
+        error: function() {
+        }
+    });
 }
